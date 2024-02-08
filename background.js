c90b180a1027bf7c279094ccfe6768b87a0995a8
Unseeable871 <stonekids010204@gmail.com>
2024-02-08 16:19:02 -0500

    Big changes

--- background.js
+++ background.js
@@ -3,3 +3,14 @@ chrome.browserAction.onClicked.addListener(function(tab){
     'url': chrome.runtime.getURL("index.html")
   });
 });
+
+const changeNewTab = document.getElementById("changeNewTab").checked;
+if (changeNewTab == true) {
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === "complete" && tab.url === "chrome://newtab/") {
+      document.body.style.backgroundImage = "url('/images/newtab.png')";
+      document.body.style.backgroundSize = "cover";
+      document.body.style.backgroundPosition = "center";
+    }
+  });
+}
