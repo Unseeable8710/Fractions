38e2160dde2c5264b80889266e2abb0c4ba493c2
Unseeable871 <stonekids010204@gmail.com>
2024-03-16 01:43:47 -0400

    Update

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ window.onscroll = function() {scrollProgressBar()};
 function scrollProgressBar() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("progbar").style.width = scrolled + "%";
+  var scrolled = (winScroll / height) * 98.5;
+  document.getElementById("progbar").style.width = scrolled + "vw";
 }
